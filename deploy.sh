04a7cf0971f1f553972157f3ce355eab12e28bf7
Kiran Uppinkudru <kiran.uppinkudru@gmail.com>
2021-11-29 16:04:27 +0530

    Corrected the App name

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,5 @@ docker pull $dockerUsername/todo-app-prod:latest
 docker tag $dockerUsername/todo-app-prod:latest registry.heroku.com/ku-todo-app/web
 echo "$HEROKU_API_KEY" | docker login --username="$HEROKU_USERNAME" --password-stdin registry.heroku.com
 heroku container:login
-docker push registry.heroku.com/ss-todo-app/web
-heroku container:release web --app ss-todo-app
+docker push registry.heroku.com/ku-todo-app/web
+heroku container:release web --app ku-todo-app
