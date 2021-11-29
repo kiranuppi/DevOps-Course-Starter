6681edb2a151db7fe0fa2bb5ea6354b31f4fd811
Kiran Uppinkudru <kiran.uppinkudru@gmail.com>
2021-11-29 18:13:15 +0530

    Fixing the deployment issue

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-docker pull $dockerUsername/todo-app-prod:latest
-docker tag $dockerUsername/todo-app-prod:latest registry.heroku.com/ku-todo-app/web
+docker pull $DOCKER_USERNAME/todo-app-prod:latest
+docker tag $DOCKER_USERNAME/todo-app-prod:latest registry.heroku.com/ku-todo-app/web
 echo "$HEROKU_API_KEY" | docker login --username="$HEROKU_USERNAME" --password-stdin registry.heroku.com
 docker push registry.heroku.com/ku-todo-app/web
 heroku container:login
