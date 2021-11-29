5d14ba22aee78fe9caef525987d3f7228ca5f83f
Kiran Uppinkudru <kiran.uppinkudru@gmail.com>
2021-11-29 18:07:00 +0530

    minor changes

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,6 @@
 docker pull $dockerUsername/todo-app-prod:latest
 docker tag $dockerUsername/todo-app-prod:latest registry.heroku.com/ku-todo-app/web
 echo "$HEROKU_API_KEY" | docker login --username="$HEROKU_USERNAME" --password-stdin registry.heroku.com
-heroku container:login
 docker push registry.heroku.com/ku-todo-app/web
+heroku container:login
 heroku container:release web --app ku-todo-app
