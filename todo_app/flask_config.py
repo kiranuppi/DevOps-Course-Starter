19f954b304ffc951fad7323400b3a032f5333d3a
Kiran Uppinkudru <kiran.uppinkudru@gmail.com>
2021-12-24 18:52:31 +0530

    Module 10 review comments -fixing the build

--- todo_app/flask_config.py
+++ todo_app/flask_config.py
@@ -2,6 +2,6 @@ import os
 class Config:
     """Base configuration variables."""
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    LOGIN_ENABLED = os.environ.get('LOGIN_ENABLED') == "True"
+    LOGIN_ENABLED = os.environ.get('LOGIN_ENABLED') == "False"
     if not SECRET_KEY:
         raise ValueError("No SECRET_KEY set for Flask application. Did you follow the setup instructions?")
