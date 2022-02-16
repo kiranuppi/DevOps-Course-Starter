a94e8f1cb37d259a60320caec61cda3571d9998e
Kiran Uppinkudru <kiran.uppinkudru@gmail.com>
2022-02-16 09:50:09 +0000

    fixed the sensitive output issue

--- terraform_env/production/outputs.tf
+++ terraform_env/production/outputs.tf
@@ -4,6 +4,7 @@ output "webapp_url" {
 
 output "cd_webhook" {
   value = "https://${azurerm_app_service.main.site_credential[0].username}:${azurerm_app_service.main.site_credential[0].password}@${azurerm_app_service.main.name}.scm.azurewebsites.net/docker/hook"
+  sensitive = true
 }
 
 output "cosmos_connection_string" {
