3956b23fc0e1ef35917e8d030b0e93d3c17d4b1f
Kiran Uppinkudru <kiran.uppinkudru@gmail.com>
2022-02-16 12:44:01 +0000

    Module-13 Project work

--- todo_app/app.py
+++ todo_app/app.py
@@ -5,6 +5,10 @@ from todo_app.managers.UserManager import *
 from todo_app.models.view import View
 from todo_app.models.card import Card
 from todo_app.models.user import User
+
+from loggly.handlers import HTTPSHandler
+from logging import Formatter
+
 # Login
 from flask_login import UserMixin, LoginManager, login_required, login_user, logout_user
 from oauthlib.oauth2 import WebApplicationClient
@@ -30,6 +34,11 @@ def create_app():
     app = Flask(__name__)
     app.config.from_object('todo_app.flask_config.Config')
     app.wsgi_app = ReverseProxied(app.wsgi_app)
+    app.logger.setLevel(app.config['LOG_LEVEL'])
+    if app.config['LOGGLY_TOKEN'] is not None:
+        handler = HTTPSHandler(f'https://logs-01.loggly.com/inputs/{app.config["LOGGLY_TOKEN"]}/tag/todo-app')
+        handler.setFormatter(Formatter("[%(asctime)s] %(levelname)s in %(module)s: %(message)s"))
+        app.logger.addHandler(handler)
 
     client_id = os.environ['GITHUB_CLIENT_ID']
     client_secret = os.environ['GITHUB_CLIENT_SECRET']
@@ -46,16 +55,19 @@ def create_app():
 
     @login_manager.user_loader
     def load_user(user_id):
+        app.logger.debug("Loading user for login......")
         return UserToLogin(user_id)
 
     @login_manager.unauthorized_handler
     def unauthenticated():
+        app.logger.debug("User is not authenticated, redirect to login......")
         return redirect(url_for('login'))
 
     @app.route('/logout')
     @login_required
     def logout():
         logout_user()
+        app.logger.info("User logged out successfully")
         session.clear()
         return redirect("https://github.com/logout")
 
@@ -86,14 +98,17 @@ def create_app():
             data=body,
             auth=(client_id, client_secret),
         )
-
+        app.logger.debug("Token url:", token_url)
         if token_response.status_code != 200:
+            app.logger.critical("Failed to get Auth Token, redirecting to login screen")
             return redirect(url_for('login'))
 
         json_data = token_response.content.decode('utf8').replace("'", '"')
         # Parse the tokens!
         client.parse_request_body_response(json_data)
         userinfo_endpoint = "{}/user".format(base_url)
+
+        app.logger.debug("User info endpoint : %s", userinfo_endpoint)
         uri, headers, body = client.add_token(userinfo_endpoint)
         userinfo_response = requests.get(uri, headers=headers, data=body)
         if userinfo_response.ok:
@@ -103,23 +118,26 @@ def create_app():
 
             if user_manager.get_total_user_count() == 0:
                 user_manager.create_user(username=current_user_name, role="admin")
+                app.logger.info("New user: %s with ADMIN role has been created", current_user_name)
 
             if (user_manager.get_total_user_count() > 0) and (
                     user_manager.get_find_user_count(qry={"username": current_user_name}) == 0):
                 user_manager.create_user(username=current_user_name, role="read")
+                app.logger.info("New user: %s with READ role has been create", current_user_name)
 
+        app.logger.info("User: %s logged in successfully", current_user_name)
         return redirect(url_for('get_index'))
 
     @app.route('/', methods=['GET'])
     @login_required
     def sendhome():
+        app.logger.debug("Login successful, redirecting to home page")
         return redirect(url_for('get_index'))
 
     # error handling for 404
     @app.errorhandler(404)
     def not_found(e):
         return render_template("error.html", error='not found!')
-
 
     # default
     @app.route('/home', methods=['GET'])
@@ -128,6 +146,7 @@ def create_app():
         cards_list = []
         items = todo.get_all_items()
         if (app.config['LOGIN_DISABLED']):
+            app.logger.debug("Login disabled, hence no authentication flow needed.")
             user_role = False
         else:
             user_role = user_manager.is_disable()
@@ -156,8 +175,10 @@ def create_app():
         )
 
         if (str(response) != ""):
+            app.logger.info("New Todo item : %s has been created", request.form['title'])
             return redirect('/home')
         else:
+            app.logger.error("Failed to create new TODO item: %s", request.form['title'])
             return render_template("error.html", error="Task creation failed ")
 
     # edit task
@@ -168,8 +189,10 @@ def create_app():
         item = todo.get_task(id=id)
         if (str(item) != ""):
             item_info = Card(item)
+            app.logger.info("Task : %s is being edited", item_info.name)
             return render_template('edit.html', task=item_info)
         else:
+            app.logger.error("Can not retrieve task information")
             return render_template("error.html", error="Can not retrieve task information")
 
     @app.route('/edit/<id>', methods=['POST'])
@@ -184,8 +207,10 @@ def create_app():
             status=request.form['status']
         )
         if (str(response) != ""):
+            app.logger.info("%s Task Updated Successfully", request.form['title'])
             return redirect('/home')
         else:
+            app.logger.error(" %s Task update failed !", request.form['title'])
             return render_template("error.html", error="Task update failed !")
 
     # delete task
@@ -195,8 +220,10 @@ def create_app():
     def delete(id):
         response = todo.delete_task(id=id)
         if str(response) != "":
+            app.logger.info("Task deleted successfully !")
             return redirect('/home')
         else:
+            app.logger.error("Failed to delete the task")
             return render_template("error.html", error="failed to delete task!")
 
     ############# UserManagement ##########################
@@ -218,8 +245,10 @@ def create_app():
         item = user_manager.get_user(id=id)
         if (str(item) != ""):
             item_info = User(item)
+            app.logger.info("User: %s is being edited", item.name)
             return render_template('editUser.html', user=item_info)
         else:
+            app.logger.error("Failed to obtain user info!")
             return render_template("error.html", error="failed to obtain user info!")
 
     @app.route('/edituser/<id>', methods=['POST'])  # Edit user
@@ -232,8 +261,10 @@ def create_app():
             role=request.form['role']
         )
         if (str(response) != ""):
+            app.logger.info("User : %s updated successfully!", request.form['username'])
             return redirect('/usermanager')
         else:
+            app.logger.error("Failed to update user : %s", request.form['username'])
             return render_template("error.html", error="failed to update user!")
 
     @app.route('/deleteuser/<id>')  # delete user
@@ -242,8 +273,10 @@ def create_app():
     def deleteuser(id):
         response = user_manager.delete_user(id=id)
         if str(response) != "":
+            app.logger.info("User: %s deleted successfully", id)
             return redirect('/usermanager')
         else:
+            app.logger.error("Failed to delete user id : %s", id)
             return render_template("error.html", error="failed to delete user!")
 
     return app
--- todo_app/flask_config.py
+++ todo_app/flask_config.py
@@ -1,7 +1,12 @@
 import os
+
+
 class Config:
     """Base configuration variables."""
     SECRET_KEY = os.environ.get('SECRET_KEY')
+    LOG_LEVEL = os.environ.get('LOG_LEVEL')
+    LOGGLY_TOKEN = os.environ.get('LOGGLY_TOKEN')
     LOGIN_DISABLED = os.environ.get('LOGIN_DISABLED') == "False"
+
     if not SECRET_KEY:
         raise ValueError("No SECRET_KEY set for Flask application. Did you follow the setup instructions?")
